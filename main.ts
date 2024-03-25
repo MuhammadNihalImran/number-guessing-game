70b65b17ab72bc6e50a7740337daaaf82471eb5f
aruto Uzumaki <narutouzo248624@gmail.com>
2024-03-25 15:34:46 +0500

    guessing game

---
+++ main.ts
@@ -0,0 +1,22 @@
+#! /usr/bin/env node
+
+import inquirer from 'inquirer'
+
+let correctnumber = Math.floor(Math.random()*10+1);
+let attemps = 10;
+do{
+let answer = await inquirer.prompt([
+    {
+        name:"answer",
+        type:"number",
+        message: `guess your number between 1-10 and only ${attemps}: `    ,
+    }
+]);
+
+       if(answer.answer === correctnumber){
+        console.log("your answer is correct!")
+       }else{
+        console.log("your answer is incorrect!")
+        attemps--;
+       }
+}while(attemps>0)
